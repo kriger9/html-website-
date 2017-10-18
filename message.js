9205de2cfb24ceb0f2c367af1f36d746dea25ccf
kriger9 <31700982+kriger9@users.noreply.github.com>
2017-10-18 10:34:15 -0500

    Add files via upload

---
+++ message.js
@@ -0,0 +1,19 @@
+﻿function threeStrokes()
+{
+	alert("Three Strokes");
+}
+	
+function fourStrokes()
+{
+	alert("Four Strokes");
+}
+
+function fiveStrokes()
+{
+	alert("Five Strokes");
+}
+
+function eightStrokes()
+{
+	alert("Eight Strokes");
+}
